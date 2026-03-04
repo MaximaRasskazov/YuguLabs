09fd7414f509190d39f7dd2d7b26d2511956bc0e
Maxima <MaximaRassksazov@gmail.com>
2026-03-04 21:35:00 +0500

    feat: открытие файла с бд

---
+++ internal/repository/db.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func SetupDatabase() *gorm.DB {
+	dbDir := "database"
+	if err := os.MkdirAll(dbDir, os.ModePerm); err != nil {
+		log.Fatalf("Не удалось создать папку: %v", err)
+	}
+
+	dbPath := filepath.Join(dbDir, "app.db")
+
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Ошибка БД: %v", err)
+	}
+
+	log.Println("✅ База данных SQLite готова")
+	return db
+}
