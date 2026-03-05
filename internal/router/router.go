cbabd195ea524479e4db882c286730497d3e976e
Maxima <MaximaRassksazov@gmail.com>
2026-03-05 10:04:19 +0500

    feat: 2 новых эндпоинта

--- internal/router/router.go
+++ internal/router/router.go
@@ -6,14 +6,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRouter(infoCtrl *controller.InfoController) *gin.Engine {
+func SetupRouter(infoCtrl *controller.InfoController, authCtrl *controller.AuthController) *gin.Engine {
 	r := gin.Default()
 
 	infoGroup := r.Group("/info")
 	{
 		infoGroup.GET("/server", infoCtrl.ServerInfo)
-		infoGroup.GET("/client", infoCtrl.ClientInfo)
 		infoGroup.GET("/database", infoCtrl.DatabaseInfo)
+		infoGroup.GET("/client", infoCtrl.ClientInfo)
+	}
+
+	authGroup := r.Group("/api/auth")
+	{
+		authGroup.POST("/register", authCtrl.Register)
+		authGroup.POST("/login", authCtrl.Login)
 	}
 
 	return r
